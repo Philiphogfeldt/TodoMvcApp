4a368d0641e4702985f7ff62ff397446e3d5ddcc
Philip Högfeldt <112342813+Philiphogfeldt@users.noreply.github.com>
2023-12-20 13:11:08 +0100

    Added test for posting todos

--- XUnitTests/UnitTest1.cs
+++ XUnitTests/UnitTest1.cs
@@ -11,6 +11,30 @@ namespace XUnitTests
 {
 	public class TodoControllerTests
 	{
-		
+		[Fact]
+		public async Task AddTodoItem_ReturnsCreatedTodoItem()
+		{
+			var mockRepo = new Mock<ITodoRepository>();
+			var newItem = new TodoItem
+			{
+				Name = "New Todo",
+				IsDone = false
+			};
+
+			mockRepo.Setup(repo => repo.AddAsync(It.IsAny<TodoItem>()))
+					.Callback<TodoItem>(item => item.Id = 1)
+					.Returns(Task.CompletedTask);
+
+			var controller = new TodoController(mockRepo.Object);
+			var result = await controller.AddTodoItem(newItem);
+
+			var actionResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+			var createdTodoItem = Assert.IsType<TodoItem>(actionResult.Value);
+
+			Assert.Equal(1, createdTodoItem.Id);
+			Assert.Equal(newItem.Name, createdTodoItem.Name);
+			Assert.Equal(newItem.IsDone, createdTodoItem.IsDone);
+		}
+
 	}
 }
